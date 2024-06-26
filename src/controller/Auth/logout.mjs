adb0554d330a64f5fd11d623f8bcfc9ef02ca831
little-fox28 <tranthjnh288@gmail.com>
2024-06-26 16:05:58 +0700

    Done feature Logout

---
+++ src/controller/Auth/logout.mjs
@@ -0,0 +1,13 @@
+function Logout(req, res) {
+    const isLoggedIn = req.signedCookies.loggedIn;
+
+    if (!isLoggedIn) {
+        return res.status(404).json({ message: "Not Logged in!" });
+    }
+
+    res.cookie("loggedIn", "false", { maxAge: 0, signed: true, secure: true, httpOnly: true })
+
+    return res.status(200).json({ message: "Logged out!" });
+}
+
+export default Logout;
--- src/controller/Auth/register.mjs
+++ src/controller/Auth/register.mjs
@@ -1,17 +1,17 @@
-import {validationResult} from "express-validator";
-import {CreateUser, FoundUser} from "../User/userController.mjs";
+import { validationResult } from "express-validator";
+import { CreateUser, FoundUser } from "../User/userController.mjs";
 
-async function Register(req, res, next) {
+async function Register(req, res) {
     const error = validationResult(req);
-    const {name, email, password} = req.body;
-    const foundUser = FoundUser({email})
+    const { name, email, password } = req.body;
+    const foundUser = FoundUser({ email })
 
     if (!error.isEmpty()) {
-        return res.status(400).json({message: error})
+        return res.status(400).json({ message: error })
     }
 
     if (foundUser) {
-        return res.status(400).json({message: "user already exists"})
+        return res.status(400).json({ message: "user already exists" })
     }
 
     const newUser = await CreateUser(name, email, password)
